0fb0900244c067dc96a08f241eddf6e09efbaa30
Leonardo Nogueira <nogueirahy@gmail.com>
2021-10-07 00:03:30 -0400

    feact: Adiciona Home Screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React, { useRef } from 'react';
+import { FlatList, View } from 'react-native';
 
-export default () => (
-  <SafeAreaView>
-    <Text>Ola mundo!!!</Text>
-  </SafeAreaView>
-);
+import CardCharacter from '../../components/CardCharacter';
+import mockData from './mockData';
+import styles from './style';
+
+export default () => {
+  const scrollRef = useRef(null);
+
+  const renderItem = ({ item }) => <CardCharacter item={item} />;
+  const keyExtractor = (item) => item.id;
+
+  return (
+    <FlatList
+      ref={scrollRef}
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      initialNumToRender={6}
+      ItemSeparatorComponent={() => <View style={styles.divider} />}
+      showsVerticalScrollIndicator={false}
+      data={mockData}
+      renderItem={renderItem}
+      keyExtractor={keyExtractor}
+    />
+  );
+};
