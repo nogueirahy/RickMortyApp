59dfb97874b92a2ce6df6a786207dfe280e3ef35
Leonardo Nogueira <nogueirahy@gmail.com>
2021-10-06 23:01:53 -0400

    chore: Added component StatusImage

---
+++ src/components/StatusImage/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  image: {
+    height: 84,
+    width: 84,
+  },
+  textImage: {
+    textAlign: 'center',
+    marginTop: 60,
+    fontSize: 12,
+    backgroundColor: 'red',
+    fontFamily: 'Overpass-Bold',
+  },
+});
